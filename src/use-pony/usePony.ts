1f1aa2a507ed220aa0ead7f3463ed82742c08711
Michael Black <michael.black@plusthrive.com>
2023-06-01 14:57:30 +1000

    Reduced motion fix
    slight change to how we handle reduced motion

--- src/use-pony/usePony.ts
+++ src/use-pony/usePony.ts
@@ -163,10 +163,8 @@ export const usePony = ({
       transition:
         // Only apply this transition when the current swipe direction is next
         // This ensures the re-ordering of items is smoother.
-        currentSwipeDirection === ActionKind.Next
-          ? `order ${
-              (noMotion || reduceMotion) ? 0 : TRANSITION_DURATION_MS / 1000 + 0.1
-            }s ease-in`
+        currentSwipeDirection === (ActionKind.Next || !(noMotion || reduceMotion))
+          ? `order ${TRANSITION_DURATION_MS / 1000 + 0.1}s ease-in`
           : 'none',
     },
   });
